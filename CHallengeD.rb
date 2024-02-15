2d5f28ab414500561f78cb4dcb3a1d28c55961fa
raymondkumamoto <160058164+raymondkumamoto@users.noreply.github.com>
2024-02-15 11:38:49 -0800

    Create CHallengeD.rb

---
+++ CHallengeD.rb
@@ -0,0 +1,130 @@
+# CHALLENGE D: Use functions to optimize the SpongeBob theme!
+
+use_bpm 136
+use_synth :piano
+
+define :purple_measure do
+  play:E4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 2
+  play:E5
+  play:Gs4
+  sleep 1
+end
+define :yellow_measure do
+  play :r
+  sleep 1
+  play:E5
+  play:Gs4
+  play:B4
+  sleep 1
+  play:B4
+  sleep 1
+  play:E5
+  play:Gs4
+  sleep 1
+end
+define :blue_measure do
+  play:B4
+  sleep 0.75
+  play:As4
+  sleep 0.25
+  play:Gs4
+  play:B4
+  sleep 0.75
+  play:Cs5
+  sleep 0.25
+  play:B4
+  sleep 1
+  play:Gs4
+  play:E5
+  sleep 1
+end
+
+live_loop :beat do
+  32.times do
+    sample :drum_heavy_kick, amp: 0.5
+    sleep 0.5
+    sample :hat_snap, amp: 0.5
+    sleep 0.5
+  end
+  stop
+end
+live_loop:background_notes do
+  play:E3, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  play:E4, amp: 0.25
+  sleep 1
+  play:B3, amp: 0.25
+  sleep 1
+  play:E4, amp: 0.25
+  sleep 1
+  
+  play:E3, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  play:E4, amp: 0.5
+  sleep 1
+  play:B3, amp: 0.5
+  sleep 1
+  play:E4, amp: 0.5
+  sleep 1
+  
+  play:E3, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  play:E4, amp: 0.75
+  sleep 1
+  play:B3, amp: 0.75
+  sleep 1
+  play:E4, amp: 0.75
+  sleep 1
+  
+  5.times do
+    play:E3, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    play:E4, amp: 1
+    sleep 1
+    play:B3, amp: 1
+    sleep 1
+    play:E4, amp: 1
+    sleep 1
+  end
+  stop
+end
+# Measure 1
+play :r
+sleep 1
+play:E4
+play:Gs4
+sleep 2
+play:E5
+play:Gs4
+sleep 1
+
+# Measure 2
+purple_measure
+
+# Measure 3
+yellow_measure
+
+# Measure 4
+yellow_measure
+
+# Measure 5
+blue_measure
+
+# Measure 6
+yellow_measure
+
+# Measure 7
+blue_measure
+
+# Measure 8
+purple_measure
+
+sample :misc_burp
